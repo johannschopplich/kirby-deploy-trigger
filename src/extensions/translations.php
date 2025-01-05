8d196bbafc7e6bb0a5d860e39f4cf00388496728
Johann Schopplich <mail@johannschopplich.com>
2025-01-05 14:02:05 +0100

    chore: update translations

--- src/extensions/translations.php
+++ src/extensions/translations.php
@@ -3,18 +3,18 @@
 return [
     'en' => [
         'johannschopplich.deploy-trigger.label' => 'Deploy Site',
-        'johannschopplich.deploy-trigger.success ' => 'Site deployed successfully'
+        'johannschopplich.deploy-trigger.success ' => 'Successfully triggered deployment'
     ],
     'de' => [
         'johannschopplich.deploy-trigger.label' => 'Seite deployen',
-        'johannschopplich.deploy-trigger.success ' => 'Seite erfolgreich deployt'
+        'johannschopplich.deploy-trigger.success ' => 'Deployment erfolgreich gestartet'
     ],
     'fr' => [
         'johannschopplich.deploy-trigger.label' => 'Déployer le site',
-        'johannschopplich.deploy-trigger.success ' => 'Site déployé avec succès'
+        'johannschopplich.deploy-trigger.success ' => 'Déploiement démarré avec succès'
     ],
     'nl' => [
         'johannschopplich.deploy-trigger.label' => 'Site implementeren',
-        'johannschopplich.deploy-trigger.success ' => 'Site succesvol geïmplementeerd'
+        'johannschopplich.deploy-trigger.success ' => 'Implementatie succesvol gestart'
     ]
 ];
