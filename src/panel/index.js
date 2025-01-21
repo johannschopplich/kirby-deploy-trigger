e997883b83831b5af05d2d4797abeeac5fb35ebe
Johann Schopplich <mail@johannschopplich.com>
2025-01-21 08:59:56 +0100

    chore: remove unused CSS import from panel index

--- src/panel/index.js
+++ src/panel/index.js
@@ -1,7 +1,6 @@
 import DeployTriggerButton from "./components/ViewButtons/DeployTriggerButton.vue";
 import { icons } from "./config/icons";
 import { legacyViewButtonMixin } from "./utils/legacy";
-import "./index.css";
 
 window.panel.plugin("johannschopplich/deploy-trigger", {
   viewButtons: {
